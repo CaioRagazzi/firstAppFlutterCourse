ed745ae42db247563a826ce446b0984082fcb1cd
Caio Ragazzi <ca.ragazzi@gmail.com>
2019-11-25 04:43:37 -0300

    continuação curso

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import './question.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print('Answer chosen!');
   }
 
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite colour?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App!'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Question(questions.elementAt(_questionIndex)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('answer 2 chosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('answer 3 chosen');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
